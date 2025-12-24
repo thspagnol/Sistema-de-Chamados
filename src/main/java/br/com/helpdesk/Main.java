f2fc9c42ced390ac8b66cafeda6fa9250073e699
thspagnol <spagnolthiago@outlook.com>
2025-12-24 04:57:40 -0300

    Atualizacao MAIN, iniciar/finmalizar atendimento

--- src/main/java/br/com/helpdesk/Main.java
+++ src/main/java/br/com/helpdesk/Main.java
@@ -33,7 +33,82 @@ public class Main {
                 System.out.println("Erro: Digite apenas números!");
                 opcao = 0; // Força voltar ao menu
             }
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("\n--- Novo Chamado ---");
+
+                    System.out.print("Título do problema: ");
+                    String titulo = scanner.nextLine();
+
+                    System.out.print("Descrição detalhada: ");
+                    String descricao = scanner.nextLine();
+
+                    System.out.print("Nome do Solicitante: ");
+                    String solicitante = scanner.nextLine();
+
+                    Prioridade prioridade = Prioridade.MEDIA;
+
+                    int novoId = repository.buscarTodos().size() + 1;
+
+                    Chamado novoChamado = new Chamado(novoId, titulo, descricao, prioridade, solicitante);
+                    repository.salvar(novoChamado);
+
+                    System.out.println("Chamado criado com ID: " + novoId);
+                    break;
+
+                case 2:
+                    System.out.println("\n--- Lista de Chamados ---");
+                    if (repository.buscarTodos().isEmpty()) {
+                        System.out.println("Nenhum chamado cadastrado.");
+                    } else {
+                        for (Chamado c : repository.buscarTodos()) {
+                            System.out.println("#" + c.getId() + " - " + c.getTitulo() + "[" + c.getStatus() + "]");
+                        }
+                    }
+                    break;
+                case 3: 
+                    System.out.println("\n--- Atualizar Status do Chamado ---");
+
+                    System.out.print("Digite o ID do chamado: ");
+                    int idBusca = Integer.parseInt(scanner.nextLine());
+
+                    Chamado chamadoEncontrado = repository.buscarPorId(idBusca);
+
+                    if (chamadoEncontrado == null) {
+                        System.out.println("ERRO: Chamado não encontrado!");
+                        break;
+                    }
+
+                    System.out.println("Chamado selecionado: " + chamadoEncontrado.getTitulo());
+                    System.out.println("Status atual: " + chamadoEncontrado.getStatus());
+
+                    System.out.println("O que deseja fazer?");                    
+                    System.out.println("1 - Iniciar Atendimento");                    
+                    System.out.println("2 - Finalizar Chamado");
+                    System.out.print("Opção: ");
+
+                    int opcaoAcao = Integer.parseInt(scanner.nextLine());
+
+                    if (opcaoAcao == 1) {
+                        service.iniciarAtendimento(chamadoEncontrado);
+                    } else if (opcaoAcao == 2) {
+                        service.finalizarChamado(chamadoEncontrado);
+                    } else {
+                        System.out.println("Opção inválida.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida!");    
+            }
         }
+
+        System.out.println("Sistema encerrado.");
     }
 }
 
